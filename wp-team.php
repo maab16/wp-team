f0454dd7ffb26f95d16d43d6d63875985282c142
Md Abu Ahsan Basir <maab.career@gmail.com>
2017-07-23 23:48:33 +0600

    Initialize wp team

---
+++ wp-team.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Team
+Plugin URI: https://github.com/maab16/wp-team
+Description: Wordpress Team Plugin
+Version: 0.1
+Author: MD Abu Ahsan Basir
+Author URI: http://www.devcodeskill.tk/author/maab16
+License: GPL v2 or higher
+License URI: License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
